959d7a1d6f3f0ac4b1945d4099ffe7df8c805b98
Jethro Schoppenhorst <jethro.schoppenhorst@gmail.com>
2024-10-14 18:48:21 -0500

    Changes pause menu to use signals
    - callbacks removed (updated in player script)
      - instances should instead connect to paused/unpaused signals to be notified
    - when the viewport loses focus, automatically pauses

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,29 +1,32 @@
 extends Node2D
 class_name PauseMenu
 
+signal paused
+signal unpaused
+
 @export var pause_layer: CanvasLayer
 var is_paused: bool
-var unpause_callback : Callable
+
+# Call this to initiate a pause.
+func pause() -> void:
+	if not is_paused:
+		pause_layer.show()
+		is_paused = true
+		paused.emit()
+# Call this to unpause.
+func unpause() -> void:
+	if is_paused:
+		pause_layer.hide()
+		is_paused = false
+		unpaused.emit()
 
 # By default hide the pause menu.
 func _ready() -> void:
 	is_paused = false
 	pause_layer.hide()
-
-# Call this to bring up the pause screen. `on_unpause` is a function ran when the game is unpaused.
-func show_pause(on_unpause: Callable) -> void:
-	unpause_callback = on_unpause
-	if on_unpause == null:
-		push_warning("Calling show_pause with no unpause callback!")
-	pause_layer.show()
-	is_paused = true
-# When unpausing call the bound callback to resume game state.
-func _on_continue_button_pressed() -> void:
-	pause_layer.hide()
-	is_paused = false
-	if unpause_callback != null:
-		unpause_callback.call()
-
+	# When the viewport loses focus, automatically pause.
+	get_viewport().focus_exited.connect(pause)
+	
 # Go to the main menu if requested.
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menus/MainMenu.tscn")

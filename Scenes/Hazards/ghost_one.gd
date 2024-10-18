eda8513787a97fe935bd5d4c254fa32dea7bbfa1
ar13l-x07 <ar13l.x07@gmail.com>
2024-10-18 11:22:04 -0500

    Add candy corn and ghost

--- Scenes/Hazards/ghost_one.gd
+++ Scenes/Hazards/ghost_one.gd
@@ -5,7 +5,7 @@ signal hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	hide()
+	$AnimatedSprite2D.animation = "dead"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
